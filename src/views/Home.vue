a651b54d3301e737faa295d5e0fe082a742f52b2
Patricio Rojas <bruj0.696@gmail.com>
2021-10-28 19:33:54 -0300

    :package: mejoras en home, direccion a grid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,20 +3,11 @@
     <h2 class="pa-4 text-center">Draw Tools</h2>
     <v-container class="grey lighten-5">
       <v-row no-gutters>
-        <v-col
-          cols="12"
-          sm="4"
-          >
-          <v-card color="purple" dark >
-            <v-card-title class="text-h5">
-              GRID
-            </v-card-title>
+        <v-col cols="12" sm="4">
+          <v-card color="purple" dark @click="goTo('grid')">
+            <v-card-title class="text-h5"> GRID </v-card-title>
 
-            <v-card-subtitle>
-              Dibujado de grilla sobre imagen
-            </v-card-subtitle>
-
-            
+            <v-card-subtitle> Dibujado de grilla sobre imagen </v-card-subtitle>
           </v-card>
         </v-col>
       </v-row>
@@ -25,11 +16,12 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { Component, Vue } from "vue-property-decorator";
 
-export default Vue.extend({
-  name: "Home",
-  components: {
-  },
-});
+@Component
+export default class Grid extends Vue {
+  public goTo(routeName: string): void {
+    this.$router.push({ name: routeName });
+  }
+}
 </script>
